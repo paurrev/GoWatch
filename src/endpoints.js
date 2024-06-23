c59a70dfee76bc920322e189834e0d7b5986e9a8
paurrev <paulreyes391@gmail.com>
2024-06-23 14:27:41 -0500

    🆕 Create new endpoints.js file and relocate endpoints

---
+++ src/endpoints.js
@@ -0,0 +1,16 @@
+import { API_KEY, account_id } from './secrets.js';
+
+export const ENDPOINTS_MOVIE = {
+  getAllMovies: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&page=1`,
+  getMoviesPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+  getAllMoviesTrending: `https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}&language=es-MX`,
+  getMoviesNowPlaying: `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1`,
+  getAllMoviesPoster: `https://api.themoviedb.org/3/movie/653346/images?api_key=${API_KEY}`,
+  getMoviesFavorites: `https://api.themoviedb.org/3/account/${account_id}/favorite/movies?api_key=${API_KEY}`,
+  addMoviesFavorites: `https://api.themoviedb.org/3/account/${account_id}/favorite?api_key=${API_KEY}`,
+  getGenresMovies: `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`,
+};
+
+export const ENDPOINTS_TV = {
+  getAllTVs: `https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}`,
+};
