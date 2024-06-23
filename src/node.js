49ed8548cb3bae05c677a88d588c1cfcf435b51b
paurrev <paulreyes391@gmail.com>
2024-06-23 14:31:25 -0500

    📝 Add document with references to some IDs

---
+++ src/node.js
@@ -0,0 +1,32 @@
+// General
+export const logoPrincipal = document.querySelector('.logo-principal')
+
+// Main Section
+export const indexSection = document.querySelector('.index-section');
+export const buttonPopular = document.querySelector('.buttons-categories__button--popular');
+export const buttonNowPlaying = document.querySelector('.buttons-categories__button--now-playing');
+export const buttonTopRated = document.querySelector('.buttons-categories__button--top-rated');
+export const trendingContainer = document.querySelector('.trending-container');
+
+
+
+// Now playing section
+export const nowPlayingContainer = document.querySelector(
+  '.now-playing-container'
+);
+export const nowPlayingContainerMain = document.querySelector(
+  '.now-playing-container--main'
+);
+export const nowPlayingSection = document.querySelector(
+  '.buttons-categories--now-playing'
+);
+
+// Popular Section
+export const popularContainer = document.querySelector(
+  '.popular-container'
+);
+
+export const test = document.querySelector('.now-playing-containerss');
+
+// Poster Section
+export const posterContainer = document.querySelector('.posters-section');
